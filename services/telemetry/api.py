302e984693f565789546ecf7bbcf36294d4685b5
Ashmita <ashmitaj16@gmail.com>
2026-09-25 22:26:36 +0530

    fixes

--- services/telemetry/api.py
+++ services/telemetry/api.py
@@ -23,8 +23,9 @@ app = FastAPI(
 app.add_middleware(
     CORSMiddleware,
     allow_origins=[
-        "http://localhost:3000",
-    ],
+    "http://localhost:3000",
+    "https://cosmos-nu-gules.vercel.app/",
+],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
